c7b5f3d08de877e4ce84fc444001821b3cebdc61
vimalkuriensam <vimalkuriensam@gmail.com>
2025-01-24 11:39:07 +0530

    update project title in index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import React from 'react';
-import FlowCanvas from './components/FlowCanvas';
-
 const App: React.FC = () => {
-  return (
-    <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
-      <FlowCanvas />
-    </div>
-  );
-};
+  return <div>App js is running</div>
+}
 
-export default App;
+export default App
