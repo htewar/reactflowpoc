0f0df741f331958a55e9e5820aca12eefc0857f8
vimalkuriensam <vimalkuriensam@gmail.com>
2025-01-27 06:45:50 +0530

    update node styling

--- src/pages/Template/container/panel/Panel.tsx
+++ src/pages/Template/container/panel/Panel.tsx
@@ -1,9 +1,22 @@
 import { FC } from "react";
 import { Node } from "../../../../components";
+import { DATA } from "../../data";
 
-const Panel:FC = () => {
+const Panel: FC = () => {
+    const rows = [...Array(Math.ceil(DATA.nodes.length / 2))]
+    const nodeRows = rows.map((_, index) => DATA.nodes.slice(index * 2, index * 2 + 2))
+    
+    const NodeLists = nodeRows.map((row, index) => (
+        <div className="row" key={index}>
+            {row.map((node, idx) => (
+                <div className="col-1-of-2" key={`${index}-${idx}`}>
+                    <Node iconName={node.icon} nodeName={node.node} />
+                </div>
+            ))}
+        </div>
+    ))
     return <div className="template__panel">
-        <Node />
+        {NodeLists}
     </div>
 }
 
---
+++ src/pages/Template/data/index.ts
@@ -0,0 +1,16 @@
+export const DATA = {
+    nodes: [
+        {
+            icon: 'Action',
+            node: 'Action'
+        },
+        {
+            icon: 'Slack',
+            node: 'Slack'
+        },
+        {
+            icon: 'Email',
+            node: 'Email'
+        }
+    ]
+}
