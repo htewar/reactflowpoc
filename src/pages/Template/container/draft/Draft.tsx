90fde754e86ce4deca6273fee48efcc17d7e7e54
vimalkuriensam <vimalkuriensam@gmail.com>
2025-01-27 19:39:15 +0530

    added node drag feature

--- src/pages/Template/container/draft/Draft.tsx
+++ src/pages/Template/container/draft/Draft.tsx
@@ -1,59 +1,59 @@
 import { useDrop, XYCoord } from "react-dnd";
-import { Background, BackgroundVariant, Controls, Edge, EdgeChange, MiniMap, Node, NodeChange, NodePositionChange, ReactFlow, useReactFlow } from "reactflow"
+import { applyNodeChanges, Background, BackgroundVariant, Controls, Edge, MiniMap, Node, NodeChange, ReactFlow, ReactFlowInstance } from "reactflow"
 import { CustomNodeData, DraggableItem } from "../../../../types";
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useState } from "react";
+import { DATA } from "../../data";
 
 const Draft = () => {
     const [nodes, setNodes] = useState<Node<CustomNodeData>[]>([]);
     const [edges, setEdges] = useState<Edge[]>([]);
-    const reactFlowInstanceRef = useRef(false);
-    const reactFlowWrapper = useRef<HTMLDivElement | null>(null);
-    const { screenToFlowPosition } = useReactFlow()
-
-    const [collectedProps, drop] = useDrop<DraggableItem>(() => ({
-        accept: "block",
-        drop: (item, monitor) => {
-            const clientOffset = monitor.getClientOffset();
-            addNode(item, clientOffset)
-        }
-    }))
-
-    const onFlowInit = useCallback(() => {
-        reactFlowInstanceRef.current = true;
-    }, [])
+    const [rfInstance, setRfInstance] = useState<ReactFlowInstance<Node, Edge> | null>(null);
 
     const addNode = (item: DraggableItem, position: XYCoord | null) => {
-        if (!reactFlowInstanceRef.current) return;
+        if (!rfInstance) return;
+        const droppedItemId = item.itemId;
+        const droppedItem = DATA.nodes.slice().find(node => node.id == droppedItemId);
+        if (!droppedItem) return;
         if (position?.x && position?.y) {
-            setNodes(prevState => {
+            const pos = rfInstance?.project({ x: position.x - 370, y: position.y - 80 })
+            if (pos?.x && pos?.y) {
                 const newNode = {
-                    id: (prevState.length + 1).toString(),
-                    position: screenToFlowPosition({ x: position.x, y: position.y }),
-                    data: { label: `Node ${prevState.length + 1}` },
+                    id: (nodes.length + 1).toString(),
+                    position: { x: pos.x, y: pos.y },
+                    data: { 
+                        label: `Node ${nodes.length + 1}`,
+                        icon: droppedItem?.icon
+                    },
                 }
-                return [...prevState, newNode]
-            })
+                setNodes(prevState => [...prevState, newNode])
+            }
         }
     }
 
+    const handleInit = useCallback((instance: ReactFlowInstance) => {
+        instance.setViewport({ x: 0, y: 0, zoom: 0.75 });
+        setRfInstance(instance);
+    }, []);
+
+    const [_, drop] = useDrop<DraggableItem>({
+        accept: "block",
+        drop: (item, monitor) => addNode(item, monitor.getClientOffset())
+    })
+
     const onHandleNodesChange = useCallback((changes: NodeChange[]) => {
-        setNodes((prevNodes) => {
-            return prevNodes.map((node) => {
-                const change = changes.find((c): c is NodePositionChange => 'id' in c && c.id === node.id);
-                if (change) {
-                    return { ...node, ...change };
-                }
-                return node;
-            });
-        });
-    }, [setNodes])
+        setNodes(prevState => applyNodeChanges(changes, prevState))
+    }, [])
 
-    const onHandleEdgesChange = useCallback((changes: EdgeChange[]) => { }, [setEdges])
-
-    return <ReactFlow nodes={nodes} edges={edges} onInit={onFlowInit} ref={node => {
-        reactFlowWrapper.current = node;
-        drop(node);
-    }}>
+    return <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onInit={handleInit}
+        onNodesChange={onHandleNodesChange}
+        ref={drop}
+        defaultViewport={{ x: 0, y: 0, zoom: 0.5 }}
+        minZoom={0.5}
+        maxZoom={2}
+    >
         <Controls />
         <Background variant={BackgroundVariant.Lines} gap={10} color="#e9e9e9" id="1" />
         <Background variant={BackgroundVariant.Lines} gap={100} color="#cccccc" id="2" />
